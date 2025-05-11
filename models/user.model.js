b2a2269e7c4d1396a9ceb2d6d7877cc54ac8bcd2
Maksim Mitserskyi <m.maksim20051122@gmail.com>
2025-05-11 19:47:28 +0300

    rewrite models

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+//  Maks
+const db = require('./db');
+
+const User = function (user) {
+    this.id = user.id;
+};
+
+User.getAll = async () => {
+    const query = 'SELECT * FROM users';
+    const rows = await db.query(query);
+    return rows;
+};
+
+User.postUser = async (name, email) => {
+    const query = 'INSERT INTO users (name, email) VALUES (?, ?)';
+    const result = await db.query(query, [name, email]);
+    return result;
+};
+
+User.deleteUser = async (id) => {
+    const query = 'DELETE FROM users WHERE id = ?';
+    const result = await db.query(query, [id]);
+    return result;
+};
+
+module.exports = User;
