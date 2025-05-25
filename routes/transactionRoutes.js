fa8e80dd2e055fed24613202b53a503d08fc4148
Maksim Mitserskyi <m.maksim20051122@gmail.com>
2025-05-25 13:21:44 +0300

    err fix

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db.config');
+const db = require('../config/db.config');//updated
 
 router.get('/report', async (req, res) => {
   try {
